2482f717e6a1a30fce2d774f4256ec32c09c3d23
Gus <gusta1296@gmail.com>
2020-07-28 21:49:52 -0700

    first push

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
+import HomePage from './homepage.component';
 
-class App extends Component {
-  render() {
-    return <div className='App'>Hello World</div>;
-  }
+function App(){
+  return(
+    <div>
+      <HomePage/>
+    </div>
+  )
 }
 
 export default App;
